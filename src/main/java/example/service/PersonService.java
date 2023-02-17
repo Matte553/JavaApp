2d42d5892b757e86ade2a424b874ba7af81620d2
Matte553 <91864594+Matte553@users.noreply.github.com>
2023-02-17 15:25:41 +0100

    Added method for retrieving all persons from database.

--- src/main/java/example/service/PersonService.java
+++ src/main/java/example/service/PersonService.java
@@ -1,8 +1,10 @@
 package example.service;
 
 import Entities.*;
-
 import example.api.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -12,9 +14,10 @@ import java.util.Optional;
 
 @Service
 public class PersonService {
-    private List<Person> personList;
+    private ArrayList<Person> personList;
 
-    public PersonService(){
+    public PersonService() throws Exception {
+        /*
         personList = new ArrayList<>();
         Person person1 = new Person(1, "Anton", "Holm", "anton1@gmail.com");
         Person person2 = new Person(2, "Anton", "Nor", "anton2@gmail.com");
@@ -22,7 +25,28 @@ public class PersonService {
         //Person person4 = new Person(x,x,x,x)
 
         personList.addAll(Arrays.asList(person1, person2, person3));
+        */
+        this.retrievePersonsFromDB();
+
     }
+
+    public void retrievePersonsFromDB() throws Exception {
+        SessionFactory sessionFactory = HibernateSetup.getSessionFactory(); // Initiera en koppling för databasen.
+        Session session = sessionFactory.openSession();                     // Skapa en session för koppling.
+        session.beginTransaction();
+
+        Query query = session.createQuery(("from PersonEntity "));
+        List<PersonEntity> list=query.list();
+        list.forEach(System.out::println);
+
+        personList = new ArrayList<>();
+
+        for(PersonEntity p: list ) {
+            Person person = new Person(p.getId(), p.getFirstname(), p.getLastname(), p.getMail());
+            personList.add(person);
+        };
+    }
+
     public Optional<Person> getPerson(Integer id){
         Optional optional = Optional.empty();
         for (Person person: personList){
