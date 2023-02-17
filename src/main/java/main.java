e6f101a6f32e3407ab44be48b377486bfd774f7a
Anton <anton.homm@gmail.com>
2023-02-17 11:36:40 +0100

    Intitial test

---
+++ src/main/java/main.java
@@ -0,0 +1,20 @@
+package ;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GitarrAppApi17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitarrAppApi17Application.class, args);
+    }
+    @Bean
+    ApplicationRunner applicationRunner(){
+        return args -> {
+            System.out.println("Hello world!");
+        };
+    }
+}
