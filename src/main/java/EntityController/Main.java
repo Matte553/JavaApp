9db913955dab276f19cd57c9dc598d00f36047a8
Matte553 <91864594+Matte553@users.noreply.github.com>
2023-02-20 13:38:42 +0100

    Updated code in main for EntityController

--- src/main/java/EntityController/Main.java
+++ src/main/java/EntityController/Main.java
@@ -7,11 +7,20 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        // New Controller
         EntityController ec = new EntityController();
 
+        // Read from all database tables
         ArrayList<PersonEntity> persons         = ec.getPersons();
         ArrayList<ChatEntity> chats             = ec.getChats();
         ArrayList<ChatmemberEntity> chatMembers = ec.getChatMembers();
         ArrayList<MessageEntity> messages       = ec.getMessages();
+
+        // Iterate through list
+        for (PersonEntity p: persons) {
+            System.out.println(p);
+        }
+
+
     }
 }
--- src/main/java/example/main.java
+++ src/main/java/example/main.java
@@ -1,11 +1,4 @@
 package example;
-
-import Entities.HibernateSetup;
-import Entities.PersonEntity;
-import example.api.model.Person;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,21 +12,7 @@ public class main {
     public static void main(String[] args) throws Exception {
 
         SpringApplication.run(main.class, args);
-        SessionFactory sessionFactory = HibernateSetup.getSessionFactory(); // Initiera en koppling för databasen.
-        Session session = sessionFactory.openSession();                     // Skapa en session för koppling.
-        session.beginTransaction();
-
-        Query query = session.createQuery(("from PersonEntity "));
-        List<PersonEntity> list=query.list();
-        list.forEach(System.out::println);
-
-        for(PersonEntity person: list ) {
-            person.getId();//Loadsa stuff
-        };
-
-
     }
-
 
     @Bean
     ApplicationRunner applicationRunner(){
