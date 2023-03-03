3a6ffe12c0b981202366aad880790e644edb463d
Adnan <you@example.com>
2023-03-03 15:03:24 +0100

    created a part of instrument page

--- src/main/webapp/resources/scripts/script.js
+++ src/main/webapp/resources/scripts/script.js
@@ -9,6 +9,15 @@ $(document).ready(function () {
         })
         $(this).addClass('actived');
     })
+
+    showSlides(0);
+    $('#prev').on('click', function () {
+        showSlides(-1);
+    })
+    $('#next').on('click', function () {
+        showSlides(1);
+    })
+
 });
 
 /**
@@ -28,7 +37,32 @@ function clearErrorMessages(target, eventType = 'keydown') {
  * Scroll messages to bottom
  * @param id element id
  */
-function scrollToBottom(id){
+function scrollToBottom(id) {
     const element = document.getElementById(id);
     element.scrollTop = element.scrollHeight;
+}
+
+function showSlides(next) {
+    let currentId = '#current-slide';
+    let currentSlide = parseInt($(currentId).val());
+    let $slides = $('.slides');
+    let $dots = $('.dot');
+    $slides.each(function () {
+        $(this).css('display', 'none');
+    });
+    $dots.each(function () {
+        $(this).removeClass('active');
+    });
+
+    currentSlide += next;
+    if (currentSlide >= $slides.length) {
+        currentSlide = 0;
+    }
+    if (currentSlide < 0) {
+        currentSlide = $slides.length - 1;
+    }
+
+    $slides.eq(currentSlide).css('display', 'block');
+    $dots.eq(currentSlide).addClass('active');
+    $(currentId).val(currentSlide);
 }
