527b9d47a713fc6c015bccc7f15f79c78ccff1da
Matte553 <Mattiaslindblom98@gmail.com>
2023-03-07 17:29:15 +0100

    removed comments from main file

--- src/main/java/EntityController/Main.java
+++ src/main/java/EntityController/Main.java
@@ -3,15 +3,6 @@ package EntityController;
 import Entities.*;
 import java.util.ArrayList;
 
-// Log Tabell i DB: LOG(kundid, text, pictures, datetime); GÖR ER TILL STARK ENTITET FÖR AUTOINCREMENT
-
-//addMessage() now returns message
-//addInstrument() now returns instrument
-//addInstrumentPicture() now returns instrumentPicture
-//addReservation() now returns reservation
-//addReparation() now returs reparation
-//addLog() now returns log
-
 // Kalender Tabell i DB: KALENDER(id, starttid, sluttid, startdatum, slutdatum, errandNumber, ämne, fritext);
 
 
@@ -20,10 +11,6 @@ public class Main {
 
         // TESTING
         EntityController ec = new EntityController();
-
-        //ReservationEntity r = ec.addReservation(101, 3);
-        //System.out.println(r.getInstrumentId());
-
 
     }
 }
