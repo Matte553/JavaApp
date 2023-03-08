99aadb2843afce1816474eed62666e8ef7190ee7
anton <anton95@live.se>
2023-03-08 11:03:54 +0100

    Snyggade till person

--- src/main/java/api/service/PersonService.java
+++ src/main/java/api/service/PersonService.java
@@ -1,10 +1,11 @@
 package api.service;
 
-import api.model.Person;
+import api.model.PersonModel;
 import Entities.PersonEntity;
 import EntityController.EntityController;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PersonService {
@@ -13,36 +14,50 @@ public class PersonService {
     public PersonService() throws Exception {
     }
 
-    public ArrayList<Person> getPersons() throws Exception {
-        ArrayList<PersonEntity> dblist = ec.getPersons();
-        //
-        ArrayList<Person> personList = new ArrayList<>();
-        for (PersonEntity p : dblist) {
-            personList.add(new Person(p.getId(), p.getFirstname(), p.getLastname(), p.getMail(), p.getPhone(), p.getCustomerNumber()));
-        }
-        return personList;
+    private PersonModel convertPersonEntity(PersonEntity value) {
+        return new PersonModel(
+                value.getId(),
+                value.getFirstname(),
+                value.getLastname(),
+                value.getMail(),
+                value.getPhone(),
+                value.getCustomerNumber());
     }
 
-    public Person getPerson(Integer id) throws Exception{
-        ArrayList<Person> personList = this.getPersons();
-        for (Person p : personList) {
+    private List<PersonModel> convertListEntity(ArrayList<PersonEntity> dbList) {
+        List<PersonModel> apiList = new ArrayList<>();
+
+        for (PersonEntity entity : dbList) {
+            apiList.add( this.convertPersonEntity(entity));
+        }
+        return apiList;
+    }
+
+    public List<PersonModel> getAllPersons() throws Exception {
+        ArrayList<PersonEntity> dblist = ec.getPersons();
+        return this.convertListEntity(dblist);
+    }
+
+    public PersonModel getPerson(Integer id) throws Exception{
+        List<PersonModel> personModelList = this.getAllPersons();
+        for (PersonModel p : personModelList) {
             if (p.getId() == id) {
                 System.out.println(id);
                 return p;
             }
         }
         //Throw something yo, as long as it isnt this
-        return new Person(-1, "This", "person","did","not","exist");
+        return new PersonModel(-1, "This", "person","did","not","exist");
     }
 
-    public Person addPerson(Person postPerson) throws Exception {
+    public PersonModel addPerson(PersonModel postPerson) throws Exception {
         PersonEntity p = new PersonEntity(postPerson.getFname(),postPerson.getLname(),postPerson.getPhone(), postPerson.getEmail(), postPerson.getCustomerNumber());
         ec.addCustomer(p, "Random Subject");
         return postPerson;
     }
 
-    public Person getAdmin() {
+    public PersonModel getAdmin() {
         PersonEntity p = ec.getAdmin();
-        return new Person(p.getId(), p.getFirstname(), p.getLastname(), p.getMail(), p.getPhone(), p.getCustomerNumber());
+        return this.convertPersonEntity(p);
     }
 }
