d975e5e09ea3f893e39b0e7f9772bea8fc22de77
Matte553 <91864594+Matte553@users.noreply.github.com>
2023-03-09 15:33:15 +0100

    maby some changes has been mady

--- src/main/java/EntityController/Main.java
+++ src/main/java/EntityController/Main.java
@@ -11,28 +11,20 @@ public class Main {
         // TESTING
         EntityController ec = new EntityController();
 
+/*
         PersonEntity p = new PersonEntity("Torkel", "Token", "333", "TorkelToken@gmail.com", "14");
         ec.addCustomer(p, "Övrigt");
 
-        PersonEntity person = ec.getPersonWithID(6);
-        ec.addChat(person, "Reservation");
 
-        //PersonEntity person = ec.getPersonWithID(3);
+ */
+        //PersonEntity person = ec.getPersonWithID(4);
         //ec.addChat(person, "Reservation");
-
-        //int i = ec.getChatWithSubject(3, "Övrigt");
-        //System.out.println(i);
-        //ec.addMessage(1,3, "Reparation","Hej", null);
-
-        /*
-        ArrayList<MessageEntity> e = ec.getMessagesWithSubject(3, "Reparation");
-        for (MessageEntity mess: e){
+/*
+        ArrayList<MessageEntity> m = ec.getMessages(4, "Reservation");
+        for (MessageEntity mess: m){
             System.out.println(mess.getText());
-        }.
-        */
-
-
-
+        }
+*/
 
     }
 }
