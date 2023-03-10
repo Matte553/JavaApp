a308f15752bab9359bf11ceb8ea956790a413276
Anton <anton.homm@gmail.com>
2023-03-10 11:48:15 +0100

    Uncommented broken messagefunctions, time to hack around the fact that we need "Subject"

--- src/main/java/api/controller/MessageController.java
+++ src/main/java/api/controller/MessageController.java
@@ -26,19 +26,20 @@ public class MessageController {
                 .orElseThrow(() -> new MessageNotFoundException(id));
     }*/
 
+    /*
     //In Postman, GET localhost:8080/messages/1/2   for messages between person 1 and 2
     @GetMapping("/messages/{id1}")
     List<MessageModel> allBetweenTwoPeople(@PathVariable("id1") Integer id1) throws Exception {
         return messageService.getMessages(id1);
-    }
+    }*/
 
     @GetMapping("/messages/all")
     List<MessageModel> getAllMessages() throws Exception {
         return messageService.getAllMessages();
     }
-
+/*
     @PostMapping("/messages/add")
     private MessageModelPost addMessage(@RequestBody MessageModelPost postMessages) {
         return messageService.addMessage(postMessages);
-    }
+    }*/
 }
--- src/main/java/api/service/MessageService.java
+++ src/main/java/api/service/MessageService.java
@@ -45,10 +45,10 @@ public class MessageService {
         ArrayList<MessageEntity> dbList = ec.getMessages();
         return this.convertListEntity(dbList);
     }
-
+/*
     public MessageModelPost addMessage(MessageModelPost postMessage) {
         MessageEntity message = ec.addMessage(postMessage.getFromID(), postMessage.getToID(), postMessage.getText(), postMessage.getImageUrl());
         // Blev lite pannkaka här med att returnera MessageEntity -> MessageModel eftersom det är ju MessageModelPost som ska ges tillbaka till klienten.
         return postMessage;
-    }
+    }*/
 }
