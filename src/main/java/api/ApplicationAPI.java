6723a1ae0264a78520b4fb3bc58e78175e191797
anton <anton95@live.se>
2023-03-14 11:30:35 +0100

    WE HAVE MERGED!

---
+++ src/main/java/api/ApplicationAPI.java
@@ -0,0 +1,29 @@
+//API
+package api;
+
+import Entities.HibernateSetup;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class ApplicationAPI extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(applicationClass, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<ApplicationAPI> applicationClass = ApplicationAPI.class;
+}
