e842fe31e0d8c83338d78730817e8b9be184ec7a
Anton <anton.homm@gmail.com>
2023-03-14 16:53:17 +0100

    Calender add working, was a missing Requestbody parameter and sql Time/Date objects not being processed correctly

--- src/main/java/api/controller/CalenderEventController.java
+++ src/main/java/api/controller/CalenderEventController.java
@@ -1,10 +1,12 @@
 package api.controller;
 //API
 import api.model.CalenderEventModel;
+import api.model.PersonModel;
 import api.service.CalenderEventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,7 +27,8 @@ public class CalenderEventController {
 
 
     @PostMapping("/calender/add")
-    CalenderEventModel addCalenderEvent(CalenderEventModel calenderEvent) {
+    CalenderEventModel addCalenderEvent(@RequestBody CalenderEventModel calenderEvent) {
         return service.addCalenderEvent(calenderEvent);
     }
+
 }
--- src/main/java/api/service/CalenderEventService.java
+++ src/main/java/api/service/CalenderEventService.java
@@ -6,7 +6,9 @@ import EntityController.EntityController;
 import api.model.CalenderEventModel;
 import org.springframework.stereotype.Service;
 
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -20,10 +22,10 @@ public class CalenderEventService {
     private CalenderEventModel convertCalenderEntity(CalendarEventEntity value) {
         return new CalenderEventModel(
                 value.getId(),
-                value.getStartTime(),
-                value.getStopTime(),
-                value.getStartDate(),
-                value.getStopDate(),
+                value.getStartTime().toString(),
+                value.getStopTime().toString(),
+                value.getStartDate().toString(),
+                value.getStopDate().toString(),
                 value.getSubject(),
                 value.getFreeText(),
                 value.getReferenceNumber(),
@@ -43,13 +45,18 @@ public class CalenderEventService {
         ArrayList<CalendarEventEntity> dbList = ec.getCalendarEvent();
         return this.convertListEntity(dbList);
     }
-
+    private Time stringToTime(String s){
+        return new Time(s.charAt(0)+s.charAt(1),s.charAt(3)+s.charAt(4),s.charAt(6)+s.charAt(7));
+    }
+    private java.sql.Date stringToDate(String s){
+        return new java.sql.Date(s.charAt(0)+s.charAt(1)+s.charAt(2)+s.charAt(3),s.charAt(5)+s.charAt(6),s.charAt(8)+s.charAt(9));
+    }
     public CalenderEventModel addCalenderEvent(CalenderEventModel event) {
         CalendarEventEntity eventEntity = ec.addCalendarEvent(
-                event.getStartTime(),
-                event.getStopTime(),
-                event.getStartDate(),
-                event.getStopDate(),
+                stringToTime(event.getStartTime()),
+                stringToTime(event.getStopTime()),
+                stringToDate(event.getStartDate()),
+                stringToDate(event.getStopDate()),
                 event.getSubject(),
                 event.getFreeText(),
                 event.getReferenceNumber(),
